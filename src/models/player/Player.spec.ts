8f19b64836519b298ca76b5f1e1180cf0afd8978
Álvaro <alvaro.prba@gmail.com>
2023-02-28 16:05:41 +0100

    Implement Player model and tests to keep the state of a player during a match

---
+++ src/models/player/Player.spec.ts
@@ -0,0 +1,98 @@
+import { Piece } from '../piece/Piece';
+import { PieceTypes } from '../piece/PieceType';
+import PieceFactory from '../piece/PieceFactory';
+import { Player } from './Player';
+
+describe('Player', () => {
+    it('creates with the specified piece types', () => {
+        const player = Player.withPieceTypes([
+            PieceTypes.ARCHER,
+            PieceTypes.MERCENARY,
+        ]);
+
+        expect(player).toBeInstanceOf(Player);
+        expect(player['_hand'].length).toBe(0);
+        expect(player['_bag'].length).toBe(4);
+        expect(player['_recruitment']).toEqual(
+            expect.arrayContaining([
+                {
+                    type: PieceTypes.ARCHER,
+                    availableQuantity: 2,
+                },
+                {
+                    type: PieceTypes.MERCENARY,
+                    availableQuantity: 3,
+                },
+            ]),
+        );
+    });
+    it('can draw a hand from the bag', () => {
+        const player = Player.withPieceTypes([
+            PieceTypes.ARCHER,
+            PieceTypes.MERCENARY,
+        ]);
+        player.drawHand();
+        expect(player['_hand'].length).toBe(3);
+        expect(player['_bag'].length).toBe(1);
+    });
+
+    it('can recruit pieces into the bag', () => {
+        const player = Player.withPieceTypes([
+            PieceTypes.ARCHER,
+            PieceTypes.MERCENARY,
+        ]);
+        player.recruit(PieceTypes.ARCHER);
+
+        expect(player['_bag'].length).toBe(5);
+        expect(player['_recruitment']).toEqual(
+            expect.arrayContaining([
+                {
+                    type: PieceTypes.ARCHER,
+                    availableQuantity: 1,
+                },
+                {
+                    type: PieceTypes.MERCENARY,
+                    availableQuantity: 3,
+                },
+            ]),
+        );
+    });
+
+    it('can discard pieces from hand', () => {
+        const player = Player.withPieceTypes([
+            PieceTypes.ARCHER,
+            PieceTypes.MERCENARY,
+        ]);
+        player['_hand'].push(PieceFactory.pieceOfType(PieceTypes.ARCHER));
+
+        player.discard(PieceTypes.ARCHER);
+
+        expect(player['_hand'].length).toBe(0);
+        expect(player['_discard']).toEqual(
+            expect.arrayContaining([
+                PieceFactory.pieceOfType(PieceTypes.ARCHER),
+            ]),
+        );
+    });
+
+    it('can refill bag from discard', () => {
+        const player = Player.withPieceTypes([
+            PieceTypes.ARCHER,
+            PieceTypes.MERCENARY,
+        ]);
+        player['_bag'].splice(0, 4);
+        const pieces: Piece[] = [
+            PieceFactory.pieceOfType(PieceTypes.ARCHER),
+            PieceFactory.pieceOfType(PieceTypes.BERSERKER),
+            PieceFactory.pieceOfType(PieceTypes.ARCHER),
+        ];
+
+        player['_discard'] = [...pieces];
+
+        player.refill();
+
+        expect(player['_discard'].length).toBe(0);
+        expect(player['_bag'].length).toBe(pieces.length);
+        expect(player['_bag']).toEqual(expect.arrayContaining(pieces));
+    });
+});
---
+++ src/models/player/Player.ts
@@ -0,0 +1,75 @@
+import { Piece } from '../piece/Piece';
+import { PieceType } from '../piece/PieceType';
+import PieceFactory from '../piece/PieceFactory';
+
+interface RecruitablePiece {
+    type: PieceType;
+    availableQuantity: number;
+}
+
+export class Player {
+    private _bag: Piece[];
+    private _hand: Piece[];
+    private _recruitment: RecruitablePiece[];
+    private _discard: Piece[];
+
+    private setUpBag(availablePieceTypes: PieceType[]): void {
+        this._bag = [];
+        availablePieceTypes.forEach((type) => {
+            this._bag.push(PieceFactory.pieceOfType(type));
+            this._bag.push(PieceFactory.pieceOfType(type));
+        });
+    }
+
+    private setUpRecruitment(availablePieceTypes: PieceType[]) {
+        this._recruitment = [];
+        var count: number;
+        availablePieceTypes.forEach((type) => {
+            count = PieceFactory.pieceOfType(type).totalNumber - 2;
+            this._recruitment.push({ type: type, availableQuantity: count });
+        });
+    }
+
+    private constructor(availablePieceTypes: PieceType[]) {
+        this.setUpBag(availablePieceTypes);
+        this.setUpRecruitment(availablePieceTypes);
+        this._discard = Array<Piece>();
+        this._hand = Array<Piece>();
+    }
+
+    public static withPieceTypes(availablePieceTypes: PieceType[]): Player {
+        return new this(availablePieceTypes);
+    }
+
+    private _getRandomFromBag(): Piece {
+        const idx = Math.floor(Math.random() * this._bag.length);
+        return this._bag.splice(idx, 1)[0];
+    }
+
+    public drawHand(): void {
+        for (let i = 0; i < 3; i++) {
+            this._hand.push(this._getRandomFromBag());
+        }
+    }
+
+    public recruit(ofType: PieceType): void {
+        const idx = this._recruitment.findIndex((item) => item.type == ofType);
+
+        if (idx === -1 || this._recruitment[idx].availableQuantity < 1) return;
+
+        this._recruitment[idx].availableQuantity -= 1;
+        this._bag.push(PieceFactory.pieceOfType(this._recruitment[idx].type));
+    }
+
+    public discard(ofType: PieceType): void {
+        const idx = this._hand.findIndex((piece) => piece.type == ofType);
+
+        if (idx === -1) return;
+
+        this._discard.push(this._hand.splice(idx, 1)[0]);
+    }
+
+    public refill(): void {
+        this._bag = this._discard.splice(0, this._discard.length);
+    }
+}
