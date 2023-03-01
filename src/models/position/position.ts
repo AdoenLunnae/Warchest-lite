355c8c2d3a9103fbea0cff2552603353820359f7
Álvaro <alvaro.prba@gmail.com>
2023-03-01 16:07:41 +0100

    Add methods tho get adjacent and ortogonal positions from given one

--- src/models/position/position.ts
+++ src/models/position/position.ts
@@ -47,4 +47,55 @@ export class Position {
     private set y(newY: number) {
         this._y = newY;
     }
+
+    public getFromDeltaList(
+        possibilities: { deltaX: number; deltaY: number }[],
+    ): Position[] {
+        var result: Position[] = [];
+        possibilities.forEach((possibility) => {
+            try {
+                result.push(
+                    Position.fromInts(
+                        this._x + possibility.deltaX,
+                        this._y + possibility.deltaY,
+                    ),
+                );
+            } catch (e) {
+                if (
+                    !(
+                        e instanceof InvalidColNumberError ||
+                        e instanceof InvalidRowNumberError
+                    )
+                )
+                    throw e;
+            }
+        });
+        return result;
+    }
+
+    public getOrthogonallyAdjacent(): Position[] {
+        const possible = [
+            { deltaX: 1, deltaY: 0 },
+            { deltaX: -1, deltaY: 0 },
+            { deltaX: 0, deltaY: 1 },
+            { deltaX: 0, deltaY: -1 },
+        ];
+
+        return this.getFromDeltaList(possible);
+    }
+
+    public getAllAdjacent(): Position[] {
+        const possible = [
+            { deltaX: 1, deltaY: 0 },
+            { deltaX: -1, deltaY: 0 },
+            { deltaX: 0, deltaY: 1 },
+            { deltaX: 0, deltaY: -1 },
+            { deltaX: 1, deltaY: 1 },
+            { deltaX: 1, deltaY: -1 },
+            { deltaX: -1, deltaY: 1 },
+            { deltaX: -1, deltaY: -1 },
+        ];
+
+        return this.getFromDeltaList(possible);
+    }
 }
