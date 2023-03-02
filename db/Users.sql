330d7dd37c0b9fb56378f622f9a41f21912de386
Álvaro <alvaro.prba@gmail.com>
2023-03-02 14:23:28 +0100

    Add db service with mock data

---
+++ db/Users.sql
@@ -0,0 +1,16 @@
+create table Users (
+	id INT,
+	name VARCHAR(50),
+	wins INT,
+	last_seen DATE
+);
+insert into Users (id, name, wins, last_seen) values (1, 'Daria', 1, '2022-07-12 16:35:25');
+insert into Users (id, name, wins, last_seen) values (2, 'Jeremie', 7, '2022-11-16 23:02:39');
+insert into Users (id, name, wins, last_seen) values (3, 'Jamison', 10, '2022-09-20 00:25:54');
+insert into Users (id, name, wins, last_seen) values (4, 'Jenna', 10, '2022-11-16 13:58:04');
+insert into Users (id, name, wins, last_seen) values (5, 'Julie', 9, '2022-08-30 06:09:14');
+insert into Users (id, name, wins, last_seen) values (6, 'Burl', 13, '2022-03-25 23:55:22');
+insert into Users (id, name, wins, last_seen) values (7, 'Hally', 6, '2023-02-02 04:44:38');
+insert into Users (id, name, wins, last_seen) values (8, 'Siana', 12, '2022-06-18 08:40:51');
+insert into Users (id, name, wins, last_seen) values (9, 'Faina', 13, '2022-08-13 08:00:07');
+insert into Users (id, name, wins, last_seen) values (10, 'Christoforo', 8, '2022-11-23 12:08:49');
